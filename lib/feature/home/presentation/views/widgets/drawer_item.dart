efaa1854048152d04219509612f8e3088c42382a
AQ <cshazemelwazery@gmail.com>
2024-08-31 15:21:25 +0300

    add drawer_item_list_view

--- lib/feature/home/presentation/views/widgets/drawer_item.dart
+++ lib/feature/home/presentation/views/widgets/drawer_item.dart
@@ -5,13 +5,25 @@ import 'package:responsive_dash_board/core/models/drawer_item_model.dart';
 import '../../../../../core/utils/app_styles.dart';
 
 class DrawerItem extends StatelessWidget {
-  const DrawerItem({super.key,required this.drawerItemModel});
-final DrawerItemModel drawerItemModel;
+  const DrawerItem(
+      {super.key, required this.drawerItemModel, required this.active,required this.onClick});
+  final DrawerItemModel drawerItemModel;
+  final bool active;
+ final void Function()? onClick;
   @override
   Widget build(BuildContext context) {
-    return  ListTile(
-      title: Text(drawerItemModel.title,style: AppStyles.styleMedium16(context),),
-      leading: SvgPicture.asset(drawerItemModel.image),
+    return GestureDetector(
+      onTap: onClick,
+      child: ListTile(
+        title: Text(
+          drawerItemModel.title,
+          style: active
+              ? AppStyles.styleBold16(context)
+                  .copyWith(color: const Color(0xff4EB7F2))
+              : AppStyles.styleMedium16(context),
+        ),
+        leading: SvgPicture.asset(drawerItemModel.image),
+      ),
     );
   }
 }
--- lib/feature/home/presentation/views/widgets/drawer_item_list_view.dart
+++ lib/feature/home/presentation/views/widgets/drawer_item_list_view.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
 
 import '../../../../../core/models/drawer_item_model.dart';
+import '../../../../../core/utils/app_images.dart';
 import 'drawer_item.dart';
 
-class DrawerItemListView extends StatelessWidget {
+class DrawerItemListView extends StatefulWidget {
   const DrawerItemListView({
     super.key,
-    required this.items,
   });
 
-  final List<DrawerItemModel> items;
+  @override
+  State<DrawerItemListView> createState() => _DrawerItemListViewState();
+}
 
+class _DrawerItemListViewState extends State<DrawerItemListView> {
+  final List<DrawerItemModel> items = [
+    const DrawerItemModel(image: Assets.imagesDashboard, title: "Dashboard"),
+    const DrawerItemModel(
+        image: Assets.imagesMyTransctions, title: "My Transaction"),
+    const DrawerItemModel(
+        image: Assets.imagesStatistics, title: "My Statistics"),
+    const DrawerItemModel(
+        image: Assets.imagesWalletAccount, title: "Wallet Account"),
+    const DrawerItemModel(
+        image: Assets.imagesMyInvestments, title: "My Investments"),
+  ];
+  int activeIndex = 0;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -18,7 +33,20 @@ class DrawerItemListView extends StatelessWidget {
       itemBuilder: (context, index) {
         return Padding(
           padding: const EdgeInsets.only(top: 20.0),
-          child: DrawerItem(drawerItemModel: items[index]),
+          child: DrawerItem(
+            drawerItemModel: items[index],
+            active: index == activeIndex,
+            onClick: () {
+
+              if(activeIndex!= index)
+             { setState(() {
+                activeIndex = index;
+              });
+               print("wow");
+             }
+
+            },
+          ),
         );
       },
     );
